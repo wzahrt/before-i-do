d5768dbe563a59c7bd07347fb5da72caec87ec31
Luis Sanchez <89441865+SiulandCheese@users.noreply.github.com>
2024-02-15 17:19:49 -0500

    Added all main pages and correct navigation between them

--- beforeido/App.tsx
+++ beforeido/App.tsx
@@ -33,9 +33,14 @@ import {
 
 
 export type RootStackParamList = {
+  // Alphabetical Order 
+  About: undefined; 
+  Assessment: undefined; 
+  Consultation: undefined; 
   Home: undefined;
-  Profile: undefined;
   Login: undefined; 
+  Profile: undefined;
+  Results: undefined; 
   Signup: undefined; 
 };
 
@@ -69,6 +74,51 @@ export type RootStackParamList = {
 //   );
 // }
 
+
+/// ****** ALL PAGES ARE IN ALPHABETICAL ORDER. LET'S KEEP IT THAT WAY. *******
+
+type AboutScreenProps = NativeStackScreenProps<RootStackParamList, "About">;
+const AboutScreen: React.FC<AboutScreenProps> = (props) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>About Screen</Text>
+      <Button
+        title="Continue"
+        onPress={() => props.navigation.push('Login')}
+      />
+    </View>
+  );
+};
+
+
+type AssessmentScreenProps = NativeStackScreenProps<RootStackParamList, "Assessment">;
+const AssessmentScreen: React.FC<AssessmentScreenProps> = (props) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Assessment Screen</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => props.navigation.push('Home')}
+      />
+    </View>
+  );
+};
+
+
+type ConsultationScreenProps = NativeStackScreenProps<RootStackParamList, "Consultation">;
+const ConsultationScreen: React.FC<ConsultationScreenProps> = (props) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Consultation Screen</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => props.navigation.push('Home')}
+      />
+    </View>
+  );
+};
+
+
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, "Home">;
 const HomeScreen: React.FC<HomeScreenProps> = (props) => {
   return (
@@ -78,20 +128,20 @@ const HomeScreen: React.FC<HomeScreenProps> = (props) => {
         title="Go to Profile"
         onPress={() => props.navigation.push('Profile')}
       />
-    </View>
-  );
-};
-
-
-type ProfileScreenProps = NativeStackScreenProps<RootStackParamList, "Profile">; 
-const ProfileScreen: React.FC<ProfileScreenProps> = (props) => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
       <Button
-        title="Go to Home"
-        onPress={() => props.navigation.push('Home')}
+        title="Take the Assessment"
+        onPress={() => props.navigation.push('Assessment')}
       />
+      <Button
+        title="View Results"
+        onPress={() => props.navigation.push('Results')}
+      />
+      <Button
+        title="Consultation"
+        onPress={() => props.navigation.push('Consultation')}
+      />
+      <Text>Your Couple Code: 20</Text>
+
     </View>
   );
 };
@@ -157,14 +207,75 @@ const LoginScreen: React.FC<LoginScreenProps> = (props) => {
   );
 };
 
-type SignupScreenProps = NativeStackScreenProps<RootStackParamList, "Signup">; 
-const SignupScreen: React.FC<SignupScreenProps> = (props) => {
+
+type ProfileScreenProps = NativeStackScreenProps<RootStackParamList, "Profile">; 
+const ProfileScreen: React.FC<ProfileScreenProps> = (props) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Signup Screen</Text>
+      <Text>Profile Screen</Text>
       <Button
         title="Go to Home"
         onPress={() => props.navigation.push('Home')}
+      />
+    </View>
+  );
+};
+
+type ResultsScreenProps = NativeStackScreenProps<RootStackParamList, "Results">;
+const ResultsScreen: React.FC<ResultsScreenProps> = (props) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Results Screen</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => props.navigation.push('Home')}
+      />
+    </View>
+  );
+};
+
+
+type SignupScreenProps = NativeStackScreenProps<RootStackParamList, "Signup">; 
+const SignupScreen: React.FC<SignupScreenProps> = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [coupleCode, setCoupleCode] = useState('');
+
+  const handleSignUp = () => {
+    // Here you can implement your signup logic
+    console.log('Signing up...');
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Couple Code:', coupleCode);
+    // After successful login, you can navigate to the home screen
+    props.navigation.push('Login');
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Screen</Text>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1, marginVertical: 10 }}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+        style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1, marginVertical: 10 }}
+      />
+      <TextInput
+        placeholder="Couple Code - Optional"
+        value={coupleCode}
+        onChangeText={setCoupleCode}
+        style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1, marginVertical: 10 }}
+      />
+      <Button
+        title="Sign Up"
+        onPress={handleSignUp}
       />
     </View>
   );
@@ -178,13 +289,18 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  /// **** STACK NAVIGATOR IS IN ALPHABETICAL ORDER, KEEP IT THAT WAY ***** 
 
   return (
     <NavigationContainer>
           <Stack.Navigator>
-            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="About" component={AboutScreen} />
+            <Stack.Screen name="Assessment" component={AssessmentScreen} />
+            <Stack.Screen name="Consultation" component={ConsultationScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen name="Results" component={ResultsScreen} />
             <Stack.Screen name="Signup" component={SignupScreen} />
           </Stack.Navigator> 
     </NavigationContainer>
@@ -233,9 +349,6 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
 });
-
-
-
 
 
 export default App;
