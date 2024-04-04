dba3beac51b6528e30710c174b92c13281d31cdd
Luis Sanchez <89441865+SiulandCheese@users.noreply.github.com>
2024-04-04 13:17:32 -0400

    Added slider functionality to questionnaire screen, Assessment screen documentation

--- beforeido/screens/AssessmentScreen.tsx
+++ beforeido/screens/AssessmentScreen.tsx
@@ -6,17 +6,66 @@ import { RootStackParamList } from '../App'; // Import RootStackParamList from A
 type AssessmentScreenProps = NativeStackScreenProps<RootStackParamList, "Assessment">;
 
 const AssessmentScreen: React.FC<AssessmentScreenProps> = (props) => {
+  const handleNewAssessment = () => {
+    // GIVEN: User info 
+    // RETURN: none 
+    // EFFECT: change navigation to move us to first questionnaire page, cleanse previous results
+    
+    // TODO 0: Determine user 
+    // TODO 1: Clear questionnaire results 
+    // TODO 2: Push to questionnaire 1  
+    
+    props.navigation.push('Questionnaire1');
+  };
+
+  const handleContinueAssessment = () => {
+    // GIVEN: User info 
+    // RETURN: none 
+    // EFFECT: change navigation to move us to most recently unanswered page
+    
+    // TODO 0: Determine user 
+    // TODO 1: Determine most recently unanswered question 
+    // TODO 2: Push to corresponding question 
+    
+    props.navigation.push('Signup');
+  };
+
+  const handleViewResults = () => {
+    // GIVEN: User info 
+    // RETURN: none 
+    // EFFECT: change navigation to move us to results page if valid
+    
+    // TODO 0: Determine user 
+    // TODO 1: Determine if all questions are answered by user 
+    // TODO 2: Determine if all questions are answered by partner (couple code)
+    // TODO 3: If yes to everything before, push to results page 
+    // TODO 4: Else, throw error and remain on page 
+    
+    props.navigation.push('Signup');
+  };
+
   return (
     <ImageBackground
       source={require('../assets/images/relationship_dynamics.png')}
       style={styles.backgroundImage}
     >
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Assessment Screen</Text>
+        <Text>Assessment Screen</Text> 
         <Button
-          title="Go to Home"
-          onPress={() => props.navigation.push('Home')}
+          title="Begin new assessment"
+          onPress={handleNewAssessment}
         />
+        <Button
+          title="Continue previous assessment"
+          onPress={handleContinueAssessment}
+        />
+        <Button
+          title="View Results"
+          onPress={handleViewResults}
+        />
+
+        
+        
       </View>
     </ImageBackground>
   );
--- beforeido/screens/Questionnaire1Screen.tsx
+++ beforeido/screens/Questionnaire1Screen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, Button, View, StyleSheet, ImageBackground, Pressable } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App'; // Import your RootStackParamList
@@ -8,6 +8,8 @@ import Slider from '@react-native-community/slider';
 
 type Questionnaire1ScreenProps = NativeStackScreenProps<RootStackParamList, "Questionnaire1">;
 const Questionnaire1Screen: React.FC<Questionnaire1ScreenProps> = (props, navigation) => {
+  const [sliderValue1, setSliderValue1] = useState(5);
+
   return (
     <ImageBackground
       source={require('../assets/images/first_page.png')}
@@ -15,15 +17,20 @@ const Questionnaire1Screen: React.FC<Questionnaire1ScreenProps> = (props, naviga
     >
       <View style={styles.container}>
         <Text style={textStyles.text}>Personality Dynamics</Text>
+        <Text style={textStyles.text}>{sliderValue1}</Text>
         <Slider
           style={{width: 200, height: 40}}
           minimumValue={1}
           maximumValue={5}
           minimumTrackTintColor="#FFFFFF"
           maximumTrackTintColor="#000000"
+          step={1}
+          value={sliderValue1}
+          onValueChange={(value) => setSliderValue1(value)}
         />
         <Pressable 
-          onPress={() => props.navigation.push('Login')}
+          // We're going to want this to navigate us to the next page, not just to Assessment
+          onPress={() => props.navigation.push('Assessment')}
           style={textStyles.button}
           > 
           <Text>Continue</Text> 
