850df1249caf44176fe0bc35a384d23ee3f2983f
Luis Sanchez <89441865+SiulandCheese@users.noreply.github.com>
2024-04-05 18:36:47 -0400

    Added json file with all questions and questions can be parsed in questionnaire screen

--- beforeido/screens/Questionnaire1Screen.tsx
+++ beforeido/screens/Questionnaire1Screen.tsx
@@ -1,10 +1,26 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
+import React from 'react'; 
 import {Text, Button, View, StyleSheet, ImageBackground, Pressable } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App'; // Import your RootStackParamList
 import { textStyles } from '../TextStyles';
 import Slider from '@react-native-community/slider';
 import firestore from '@react-native-firebase/firestore';
+import questionsData from '../questionnaire_en.json';
+
+interface Question {
+  category: string; 
+  subcategory: string; 
+  questionNumber: number;
+  question: string;
+}
+
+const questions: Question[] = questionsData.questions.map(item => ({
+  category: item.category,
+  subcategory: item.subcategory,
+  questionNumber: item.questionNumber,
+  question: item.question
+}));
 
 
 type Questionnaire1ScreenProps = NativeStackScreenProps<RootStackParamList, "Questionnaire1">;
@@ -16,37 +32,40 @@ const Questionnaire1Screen: React.FC<Questionnaire1ScreenProps> = (props, naviga
   const [sliderValue4, setSliderValue4] = useState(5);
   const [sliderValue5, setSliderValue5] = useState(5);
 
-  const [questions, setQuestions] = useState<string[]>([]); // Save the questions from database - determine if we're loading or not
-  const [loading, setLoading] = useState(true);
-  
-  const smallestQuestion = 1; // Only values that need to be changed between screens
-  const largestQuestion = 5; 
+  var nextQuestion: number = 1; // In the future, get this value from the database
+  const loading = false; 
 
-  useEffect(
-    () => { // We're going to use this effect in the code because reactNative won't let us use async otherwise
-      async function fetchData() {  // Fetching data from firestore... 
-        try {
-          const querySnapshot = await firestore()
-            .collection('questions')
-            .where('questionNumber', '>=', smallestQuestion)
-            .where('questionNumber', '<=', largestQuestion)
-            .get();
-          
-          const fetchedQuestions: string[] = [];
-          querySnapshot.forEach((doc) => {
-            const question = doc.data().question;
-            fetchedQuestions.push(question);
-          });
+  // const [questions, setQuestions] = useState<string[]>([]); // Save the questions from database - determine if we're loading or not
+  // const [loading, setLoading] = useState(true);
   
-          setQuestions(fetchedQuestions); // By using setQuestions and setLoading, we can change these values past their inital establishment!! 
-          setLoading(false);
-        } catch (error) {
-          console.error('Error getting documents: ', error);
-        }
-      }
+  // const smallestQuestion = 1; // Only values that need to be changed between screens
+  // const largestQuestion = 5; 
+
+  // useEffect(
+  //   () => { // We're going to use this effect in the code because reactNative won't let us use async otherwise
+  //     async function fetchData() {  // Fetching data from firestore... 
+  //       try {
+  //         const querySnapshot = await firestore()
+  //           .collection('questions')
+  //           .where('questionNumber', '>=', smallestQuestion)
+  //           .where('questionNumber', '<=', largestQuestion)
+  //           .get();
+          
+  //         const fetchedQuestions: string[] = [];
+  //         querySnapshot.forEach((doc) => {
+  //           const question = doc.data().question;
+  //           fetchedQuestions.push(question);
+  //         });
+  
+  //         setQuestions(fetchedQuestions); // By using setQuestions and setLoading, we can change these values past their inital establishment!! 
+  //         setLoading(false);
+  //       } catch (error) {
+  //         console.error('Error getting documents: ', error);
+  //       }
+  //     }
         
-      fetchData(); // Calling the function we just made... 
-    }, []); // Everything before this was the first parameter of useEffect, and [] is the second.   
+  //     fetchData(); // Calling the function we just made... 
+  //   }, []); // Everything before this was the first parameter of useEffect, and [] is the second.   
 
 
   return (
@@ -63,7 +82,7 @@ const Questionnaire1Screen: React.FC<Questionnaire1ScreenProps> = (props, naviga
         ) : (
           <>
 
-          <Text style={textStyles.text}>{questions[0]}</Text>
+          <Text style={textStyles.text}>{questions[0].question}</Text>
           <Text style={textStyles.text}>{sliderValue1}</Text>
           <Slider
             style={{width: 200, height: 40}}
@@ -76,7 +95,7 @@ const Questionnaire1Screen: React.FC<Questionnaire1ScreenProps> = (props, naviga
             onValueChange={(value) => setSliderValue1(value)}
           />
 
-          <Text style={textStyles.text}>{questions[1]}</Text>
+          <Text style={textStyles.text}>{questions[1].question}</Text>
           <Text style={textStyles.text}>{sliderValue2}</Text>
           <Slider
             style={{width: 200, height: 40}}
@@ -89,7 +108,7 @@ const Questionnaire1Screen: React.FC<Questionnaire1ScreenProps> = (props, naviga
             onValueChange={(value) => setSliderValue2(value)}
           />
 
-          <Text style={textStyles.text}>{questions[2]}</Text>
+          <Text style={textStyles.text}>{questions[2].question}</Text>
           <Text style={textStyles.text}>{sliderValue3}</Text>
           <Slider
             style={{width: 200, height: 40}}
@@ -102,7 +121,7 @@ const Questionnaire1Screen: React.FC<Questionnaire1ScreenProps> = (props, naviga
             onValueChange={(value) => setSliderValue3(value)}
           />
 
-          <Text style={textStyles.text}>{questions[3]}</Text>
+          <Text style={textStyles.text}>{questions[3].question}</Text>
           <Text style={textStyles.text}>{sliderValue4}</Text>
           <Slider
             style={{width: 200, height: 40}}
@@ -115,7 +134,7 @@ const Questionnaire1Screen: React.FC<Questionnaire1ScreenProps> = (props, naviga
             onValueChange={(value) => setSliderValue4(value)}
           />
 
-          <Text style={textStyles.text}>{questions[4]}</Text>
+          <Text style={textStyles.text}>{questions[4].question}</Text>
           <Text style={textStyles.text}>{sliderValue5}</Text>
           <Slider
             style={{width: 200, height: 40}}
@@ -131,7 +150,7 @@ const Questionnaire1Screen: React.FC<Questionnaire1ScreenProps> = (props, naviga
         )}
 
         <Pressable 
-          // We're going to want this to navigate us to the next page, not just to Assessment
+          // We're going to want this to navigate us to change the 'next question' value
           onPress={() => props.navigation.push('Assessment')}
           style={textStyles.button}
           > 
